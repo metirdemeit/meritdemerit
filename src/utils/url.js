34f19e123356e83b2f58f25d76e6c5c29b483dc6
mQ <220107106@stu.sdu.edu.kz>
2026-08-06 14:29:33 +0500

    Update backend URL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,2 @@
-export const MAIN_URL = 'https://dem-1-w8zo.onrender.com';
-export const EXAMPLE_URL = 'https://dem-1-w8zo.onrender.com';
+export const MAIN_URL = 'https://meritdemerit.onrender.com';
+export const EXAMPLE_URL = 'https://meritdemerit.onrender.com';
